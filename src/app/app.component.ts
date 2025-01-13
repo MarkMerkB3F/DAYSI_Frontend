a07590006c6606dbd05b344d0bce20cc38079c03
Marius <marius.rbdj@yahoo.de>
2025-01-13 15:27:21 +0100

    Added widgets and styling and a big load of "du müffelst"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ReportsComponent} from "./components/widgets/reports/reports.component";
+import {TasksComponent} from "./components/widgets/tasks/tasks.component";
+import {CalendarComponent} from "./components/widgets/calendar/calendar.component";
+import {AddComponent} from "./components/widgets/add/add.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReportsComponent, TasksComponent, CalendarComponent, AddComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
