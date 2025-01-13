54e2a1650519b7e85b9322133c681f6fc030b6d2
LancerCreep <thiemo.n@yahoo.com>
2025-01-13 15:33:15 +0100

    added navbar component and component folder

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
