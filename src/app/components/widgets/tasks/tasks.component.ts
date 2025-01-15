8bfac8a2d6803bd67d1d8554ba1c42222bca37fb
The Sleeper <IanSchenk@gmx.de>
2025-01-15 08:52:20 +0100

    http get Request for todo

--- src/app/components/widgets/tasks/tasks.component.ts
+++ src/app/components/widgets/tasks/tasks.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { TodoService } from '../../../services/todo.service';
 
 @Component({
+  standalone: true,
   selector: 'app-tasks',
   imports: [],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
-
+  constructor(private todoService: TodoService){
+   this.todoService.getAllTodos()
+  }
 }
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+  getAllTodos(){
+    this.http.get("http://localhost:8080/todos", {responseType: 'text'}).subscribe(todos=>(
+      console.log("Todos wurden empfangen"+todos)
+ 
+    ))
+  }
+}
