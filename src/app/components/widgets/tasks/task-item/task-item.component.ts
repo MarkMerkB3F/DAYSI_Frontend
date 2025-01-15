cc8023bc5fbe6f25e35735656e84b557ca01aead
LancerCreep <thiemo.n@yahoo.com>
2025-01-15 11:06:34 +0100

    added context button and drop down functionality

--- src/app/components/widgets/tasks/task-item/task-item.component.ts
+++ src/app/components/widgets/tasks/task-item/task-item.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import {MatIcon} from '@angular/material/icon';
+import { MatIcon } from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {NgIf} from "@angular/common";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
 
 @Component({
-  selector: 'app-task-item',
-  imports: [
-    MatIcon
-  ],
-  templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.scss'
+    selector: 'app-task-item',
+    imports: [
+        MatIcon,
+        MatIconButton,
+        NgIf,
+        MatMenuTrigger,
+        MatMenu,
+        MatMenuItem,
+    ],
+    templateUrl: './task-item.component.html',
+    styleUrls: ['./task-item.component.scss'],
 })
 export class TaskItemComponent {
+    task = {
+        name: 'Task 1',
+        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ',
+    };
 
+    isExpanded = false;
+
+    toggle() {
+        this.isExpanded = !this.isExpanded;
+    }
 }
