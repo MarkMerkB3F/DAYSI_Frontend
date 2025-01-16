562065181f5f75ceeca59bd2ef998c51ce38f617
MarkMerkB3F <mcmerle@gmx.de>
2025-01-16 10:56:23 +0100

    feature branch für kalender - mm

--- src/app/components/widgets/calendar/calendar.component.ts
+++ src/app/components/widgets/calendar/calendar.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy,Component, model } from '@angular/core';
 import {Router} from "@angular/router";
+import {MatCalendar, MatDatepickerModule} from "@angular/material/datepicker";
+import {MatCard, MatCardModule} from "@angular/material/card";
+import {MatNativeDateModule, provideNativeDateAdapter} from "@angular/material/core";
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [
+    MatCalendar,
+    MatCard,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule
+  ],
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.scss'
+  styleUrl: './calendar.component.scss',
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CalendarComponent {
+  selected = model<Date | null>(null);
   constructor(private router: Router) {}
 
   openCalendarSite(){
